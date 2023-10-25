5a597083c8e02c6ae6d29964369d9f743470e8cf
Florian <70607158+Fdpr@users.noreply.github.com>
2023-10-26 02:23:57 +0300

    2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,32 +2,39 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '123456'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addName = (e) => {
     e.preventDefault()
-    if (persons.findIndex(p => p.name === newName) === -1) setPersons(persons.concat({name: newName, number: newNumber}))
+    if (persons.findIndex(p => p.name === newName) === -1) setPersons(persons.concat({ name: newName, number: newNumber }))
     else alert(`${newName} is already in the phone book!`)
   }
 
   const handleFieldChange = (e) => setNewName(e.target.value)
   const handleNumberChange = (e) => setNewNumber(e.target.value)
+  const handleFilterChange = (e) => setNewFilter(e.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input onChange={handleFilterChange}></input></div>
+      <h2>Add a new number</h2>
       <form onSubmit={addName}>
-        <div>name: <input onChange={handleFieldChange}/></div>
-        <div>number: <input onChange={handleNumberChange}/></div>
+        <div>name: <input onChange={handleFieldChange} /></div>
+        <div>number: <input onChange={handleNumberChange} /></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {persons.filter((p) => p.name.toLowerCase().startsWith(newFilter.toLocaleLowerCase())).map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
