5d00690036c9760cdab9900b47c3b89f564556c5
Florian <70607158+Fdpr@users.noreply.github.com>
2023-10-25 22:10:48 +0300

    2.2

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,7 +1,9 @@
 const Course = ({course}) => {
+    console.log(course.parts.map(part => part.exercises))
     return <div>
         <h1>{course.name}</h1>
         {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+        <p>{course.parts.map(part => part.exercises).reduce((acc, val) => acc + val)}</p>
     </div>
 }
 
