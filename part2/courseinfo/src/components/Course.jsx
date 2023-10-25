7e5e93a8b0c73d75838a53837c3e895e401a81df
Florian <70607158+Fdpr@users.noreply.github.com>
2023-10-26 01:38:41 +0300

    2.5 correction

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -3,7 +3,7 @@ const Course = ({course}) => {
     return <div>
         <h1>{course.name}</h1>
         {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
-        <p>{course.parts.map(part => part.exercises).reduce((acc, val) => acc + val)}</p>
+        <p>total number of exercises: {course.parts.map(part => part.exercises).reduce((acc, val) => acc + val)}</p>
     </div>
 }
 
