2e839926c6e9c39acc31030e5bd4060fd1e9c81d
Florian <70607158+Fdpr@users.noreply.github.com>
2023-10-27 18:41:23 +0300

    2.20

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -4,19 +4,41 @@ import finland from './finland'
 
 const baseURL = "https://studies.cs.helsinki.fi/restcountries/api"
 
-const CountryData = ({ country }) => <div>
-  <h2>{country.name.common}</h2>
-  <p>
-    Capital: {country.capital[0]}
-    <br />
-    Area: {country.area}
-  </p>
-  <h3>languages</h3>
-  <ul>
-    {Object.keys(country.languages).map(k => <li key={k}>{country.languages[k]}</li>)}
-  </ul>
-  <img src={country.flags.png} alt={country.flags.alt}></img>
-</div>
+const CountryList = ({ countries, handleShow }) => {
+  if (countries.length < 2) return null
+  else if (countries.length > 10) return <div>Too many matches, please specify another filter</div>
+  else
+    return <div>
+      {countries.map(country => <div key={country.name.common}>{country.name.common}
+        <button onClick={() => handleShow(country)}>show</button>
+      </div>)}
+    </div>
+}
+
+const WeatherData = ({ country, weather }) => <>
+  <h3>Weather in {country.capital[0]}</h3>
+  <p>temperature {weather.main.temp} </p>
+  <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+  <p>wind {weather.wind.speed}</p>
+</>
+
+const CountryData = ({ country }) => {
+  if (country)
+    return <div>
+      <h2>{country.name.common}</h2>
+      <p>
+        Capital: {country.capital[0]}
+        <br />
+        Area: {country.area}
+      </p>
+      <h3>languages</h3>
+      <ul>
+        {Object.keys(country.languages).map(k => <li key={k}>{country.languages[k]}</li>)}
+      </ul>
+      <img src={country.flags.png} alt={country.flags.alt}></img>
+    </div>
+  else return null
+}
 
 const Filter = ({ handleChange }) => <div>
   find countries
@@ -24,31 +46,34 @@ const Filter = ({ handleChange }) => <div>
 </div>
 
 const App = () => {
-  const [filter, setFilter] = useState("")
   const [countries, setCountries] = useState([])
   const [filteredCountries, setFilteredCountries] = useState([])
+  const [country, setCountry] = useState(null)
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    setWeather(null)
+    if (country){
+      api.getCityWeather(country.capital[0])
+        .then(response => setWeather(response.data))
+    }
+  }, [country])
 
   useEffect(() => {
     api.getAllCountries().then(response => setCountries(response.data))
   }, [])
 
   const changeFilter = (e) => {
-    const newFilter = e.target.value
-    setFilter(newFilter)
-    setFilteredCountries(countries.filter(country => country.name.common.toLowerCase().includes(newFilter.toLowerCase())))
+    const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+    setCountry(filteredCountries.length === 1 ? filteredCountries[0] : null)
+    setFilteredCountries(filteredCountries)
   }
-
-  const renderCountry = () => {
-    if (filteredCountries.length === 1) return <CountryData country={filteredCountries[0]} />
-    else if (filteredCountries.length < 10 ) return filteredCountries.map(country => <div key={country.name.common}>{country.name.common}</div>)
-    else return <div>Too many matches, please specify another filter</div>
-  }
-
-
 
   return <>
     <Filter handleChange={changeFilter} />
-    {renderCountry()}
+    <CountryList countries={filteredCountries} handleShow={setCountry} />
+    <CountryData country={country} />
+    {weather && country ? <WeatherData country={country} weather={weather}/> : null}
   </>
 }
 
--- part2/dataforcountries/src/api.js
+++ part2/dataforcountries/src/api.js
@@ -1,8 +1,17 @@
 import axios from 'axios'
+import apiKey from './apiKey'
 
 const baseURL = "https://studies.cs.helsinki.fi/restcountries/api"
+const weatherURL = "http://api.openweathermap.org"
 
 const getAllCountries = () => axios.get(`${baseURL}/all`)
 const getCountry = (name) => axios.get(`${baseURL}/${name}`)
+const getCityWeather = (city) => {
+    return axios.get(`${weatherURL}/geo/1.0/direct?q=${city}&appid=${apiKey}`)
+        .then(response => {
+            const {lat, lon} = response.data[0]
+            return axios.get(`${weatherURL}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
+        })
+}
 
-export default {getAllCountries, getCountry}
+export default {getAllCountries, getCountry, getCityWeather}
