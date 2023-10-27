48eb096148790aa9e6bd20ced3e0e442e5e17239
Florian <70607158+Fdpr@users.noreply.github.com>
2023-10-27 14:51:26 +0300

    2.16

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,6 +2,19 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import api from './api'
 
+const Message = ({message}) => {
+  const style = {
+    border: "3px solid green",
+    borderRadius: 10,
+    padding: 10,
+    color: "green",
+    backgroundColor: "lightgrey",
+
+  }
+  if (message === null) return null
+  else return <h3 style={style}>{message}</h3>
+}
+
 const Info = ({ name, number, handleDelete}) => <p>{name} {number} <button onClick={handleDelete}>delete</button></p>
 
 const Numbers = ({ persons, handleDelete }) => <>
@@ -25,6 +38,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
     api.getAll()
@@ -38,6 +52,7 @@ const App = () => {
       api.del(person.id)
         .then(() => {
           setPersons(persons.toSpliced(persons.indexOf(person), 1))
+          newMessage(`Deleted ${person.name} from the phonebook`)
         })
   }
 
@@ -46,14 +61,23 @@ const App = () => {
     const idx = persons.findIndex(p => p.name === newName)
     if (idx === -1)
       api.create({ name: newName, number: newNumber })
-        .then(response => setPersons(persons.concat(response.data)))
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          newMessage(`Added ${newName} to the phonebook`)
+        })
     else if (window.confirm(`${newName} is already in the phone book, replace the existing number with the new one?`))
       api.update({... persons[idx], number: newNumber}, persons[idx].id)
         .then(() => {
           const newPersons = [...persons]
           newPersons[idx] = {... persons[idx], number: newNumber}
           setPersons(newPersons)
+          newMessage(`Changed the number of ${newPersons[idx].name}`)
         })
+  }
+
+  const newMessage = (message) => {
+    setMessage(message)
+    setTimeout(() => setMessage(null), 5000)
   }
 
   const handleNameChange = (e) => setNewName(e.target.value)
@@ -65,6 +89,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Message message={message} />
       <Filter handleStateChange={handleFilterChange} />
       <h2>Add a new number</h2>
       <NumberForm handleSubmit={addPerson} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
